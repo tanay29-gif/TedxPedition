a515d6b36f69e103210627ce4ebe6dd5e63317d1
WebAddict-ai <rajkaushik103@gmail.com>
2026-08-02 21:54:00 +0530

    module-3 merged

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -18,6 +18,8 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+
+
 const db = getFirestore(app);
 const realtimeDb = getDatabase(app);
 
