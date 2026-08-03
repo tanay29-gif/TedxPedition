bf1e45b113b370a51a2ddaba993f0e29840916aa
Tanay <tanaykamdi29@gmail.com>
2026-08-04 02:52:10 +0530

    added the registeration page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import NoTeam from "./pages/NoTeam";
 import ParticipantDashboard from "./pages/ParticipantDashboard";
 import SignIn from "./pages/SignIn";
 import SuperAdminDashboard from './pages/SuperAdminDashboard.jsx';
+import SeedScript from "./SeedScript";
+import RenameDocument from "./Remane.jsx";
+import ThankYouPage from "./pages/ThankYouPage";
+import RegisterPage from "./pages/RegisterPage";
 
 
 function App() {
@@ -33,6 +37,8 @@ function App() {
 
       {/* Auth Login page */}
       <Route path="/login" element={<SignIn />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/thank-you" element={<ThankYouPage />} />
 
       {/* Admin Panel (Admin Auth protected) */}
       <Route
@@ -62,6 +68,8 @@ function App() {
 
       {/* Fallback route back to root */}
       <Route path="*" element={<HomeRedirect />} />
+      <Route path="/seed" element={<SeedScript />} />
+      <Route path="/rename" element={<RenameDocument />} />
     </Routes>
   );
 }
---
+++ src/Remane.jsx
@@ -0,0 +1,30 @@
+import { getFirestore, doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const db = getFirestore();
+// const auth = getAuth();
+
+export default async function renameDocument() {
+    // const user = auth.currentUser;
+    // if (!user) return console.error("No user logged in");
+
+    const oldDocRef = doc(db, "teams", "testing"); // e.g., "testing"
+    const newDocRef = doc(db, "teams", "M9qElO0pVHRLav2QRUPvw3bd22G3M9qElO0pVHRLav2QRUPvw3bd22G3"); // Your Auth UID
+
+    const docSnap = await getDoc(oldDocRef);
+
+    if (docSnap.exists()) {
+        // 1. Create the new document with the same data
+        await setDoc(newDocRef, docSnap.data());
+        
+        // 2. Delete the old document
+        await deleteDoc(oldDocRef);
+        
+        console.log("Document successfully moved to:", user.uid);
+    } else {
+        console.log("No such document found!");
+    }
+}
+
+// Call the function
+// renameDocument("testing");
